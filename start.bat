955a9443fe524e141bd49a6e6a65ef9fb0599a90
user8836893066 <3823448210549203-user8836893066@noreply.coze.cn>
2026-09-13 12:55:23 +0800

    fix: fix start.bat quoting issue for other PCs

--- start.bat
+++ start.bat
@@ -1,7 +1,7 @@
 @echo off
 cd /d "%~dp0"
 if not exist "node_modules" call pnpm install
-start "SmartQY" cmd /k "cd /d "%~dp0" && node node_modules\next\dist\bin\next dev --port 5000 --turbopack"
+start "SmartQY" cmd /k "cd /d %~dp0 && node node_modules\next\dist\bin\next dev --port 5000 --turbopack"
 echo Waiting for server...
 ping -n 8 127.0.0.1 >nul
 explorer "http://localhost:5000"
